527e2f3d8aaf4592878f7fe8d69ddd155a02bd5c
Do Duy <juchiast@gmail.com>
2018-07-04 20:22:40 +0700

    add test: simple

--- src/acquire_borrow.rs
+++ src/acquire_borrow.rs
@@ -2,10 +2,12 @@ use std::marker;
 
 use futures::prelude::*;
 
+#[derive(Debug)]
 struct Inner<T> {
     data: marker::PhantomData<T>,
 }
 
+#[derive(Debug)]
 pub struct AsyncMutex<T> {
     data: marker::PhantomData<Inner<T>>,
 }
@@ -24,10 +26,12 @@ impl<T> AsyncMutex<T> {
     }
 }
 
+#[derive(Debug)]
 pub struct AcquireFuture<T, F> {
     data: marker::PhantomData<(T, F)>,
 }
 
+#[derive(Debug)]
 pub enum AsyncMutexError<E> {
     Other(E),
 }
@@ -42,5 +46,50 @@ where
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_core::reactor::Core;
+
+    struct NumCell {
+        num: usize,
+    }
+
+    #[test]
+    fn simple() {
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
+
+        let async_mutex = AsyncMutex::new(NumCell { num: 0 });
+
+        let task1 = async_mutex.acquire(|ref mut num_cell| -> Result<(), ()> {
+            num_cell.num += 1;
+            Ok(())
+        });
+
+        handle.spawn(task1.map_err(|_| ()));
+
+        {
+            let _ = async_mutex.acquire(|ref mut num_cell| -> Result<(), ()> {
+                num_cell.num += 1;
+                Ok(())
+            });
+
+            let _ = async_mutex.acquire(|ref mut num_cell| -> Result<(), ()> {
+                num_cell.num += 1;
+                Ok(())
+            });
+        }
+
+        let task2 = async_mutex.acquire(|ref mut num_cell| -> Result<_, ()> {
+            num_cell.num += 1;
+            let num = num_cell.num;
+            Ok(num)
+        });
+
+        assert_eq!(core.run(task2).unwrap(), 2);
     }
 }
